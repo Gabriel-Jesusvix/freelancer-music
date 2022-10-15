f1c9b22f0760c57a216da2b46796257b41e14280
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2022-10-15 11:48:16 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      transparent: "transparent",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7C7C8A",
+        200: "#C4C4CC",
+        100: "#E1E1E6",
+      },
+      cyan: {
+        500: "#81D8F7",
+        300: "#9BE1FB",
+      },
+
+      "black-900": "#000000",
+      "white-FFFFFF": "#FFFFFF",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
